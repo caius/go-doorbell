1710b8ec7d38e67fd96a24a00e7074b7a471c704
Caius Durling <dev@caius.name>
2018-03-25 14:34:59 +0100

    Refactor Event
    Now represents a ring event

--- internal/doorbell/event_test.go
+++ internal/doorbell/event_test.go
@@ -4,11 +4,38 @@ import (
 	"testing"
 )
 
-func TestNewEventNow(t *testing.T) {
+func TestNewEvent(t *testing.T) {
 
-	e := NewEventNow()
-	if e.PressedAt.IsZero() {
-		t.Fatal("Expected event to have non-zero time")
+	e := NewEvent()
+	if e.Value != 0 {
+		t.Fatal("Expected value to be zero")
+	}
+}
+
+func TestNewPressEvent(t *testing.T) {
+	e := NewPressEvent()
+
+	if e.Value != 1 {
+		t.Fatalf("Expected value to be 1, got %d", e.Value)
+	}
+}
+
+func TestNewDepressEvent(t *testing.T) {
+	e := NewDepressEvent()
+
+	if e.Value != 0 {
+		t.Fatalf("Expected value to be 0, got %d", e.Value)
+	}
+}
+
+func TestRingRing(t *testing.T) {
+	press := NewPressEvent()
+	if press.RingRing() != true {
+		t.Fatal("Expected press to RingRing")
 	}
 
+	depress := NewDepressEvent()
+	if depress.RingRing() != false {
+		t.Fatal("Expected press NOT to RingRing")
+	}
 }
