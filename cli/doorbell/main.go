7cb3184edf40474db16f4f3b6d82e7b582d78d76
Caius Durling <dev@caius.name>
2018-03-25 02:28:17 +0100

    Start on the cli interface

---
+++ cli/doorbell/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	// "fmt"
+	log "github.com/sirupsen/logrus"
+	"os"
+	// "os/signal"
+	// "time"
+)
+
+type Configuration struct {
+	Pin        int
+	Name       string
+	MQTTBroker string
+	Valid      bool
+	Verbose    bool
+}
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	// Configuration Section
+	config := Configuration{Valid: true}
+
+	// Doorbell Configuration
+	flag.StringVar(&config.Name, "name", "", "Name for emitted events")
+	flag.IntVar(&config.Pin, "pin", 0, "GPIO Pin (Physical number) to listen for doorbell on")
+
+	// MQTT Configuration
+	flag.StringVar(&config.MQTTBroker, "mqtt-broker", "", "MQTT server (eg mqtt.local:1883)")
+
+	// Other configuration
+	flag.BoolVar(&config.Verbose, "verbose", false, "Verbose output")
+
+	flag.Parse()
+
+	log.Info("Welcome to doorbell, where we will be greeting visitors shortly.")
+
+	if config.Verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	// Check configuration is correct
+	if config.Name == "" {
+		log.Error("--name is a required argument")
+		config.Valid = false
+	}
+
+	if config.Pin == 0 {
+		log.Error("--pin is a required argument")
+		config.Valid = false
+	}
+
+	if config.MQTTBroker == "" {
+		log.Error("--mqtt-broker is a required argument")
+		config.Valid = false
+	}
+
+	if config.Valid != true {
+		log.Error("Configuration errors, please fix. Check logs for more information.")
+		os.Exit(1)
+	}
+
+}
