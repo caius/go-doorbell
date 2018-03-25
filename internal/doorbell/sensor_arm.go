cb6b30b721c412de078af397d3679f4d2ee86a4b
Caius Durling <dev@caius.name>
2018-03-25 14:21:54 +0100

    Trigger doorbell press on pin going high

--- internal/doorbell/sensor_arm.go
+++ internal/doorbell/sensor_arm.go
@@ -2,6 +2,7 @@ package doorbell
 
 import (
 	"github.com/brian-armstrong/gpio"
+	log "github.com/sirupsen/logrus"
 )
 
 // Listens for the given pin going HIGH
@@ -9,13 +10,17 @@ func (s *Sensor) listenForPress() {
 	log.Infof("Arm sensor listening to pin %d for press", s.Pin)
 
 	watcher := gpio.NewWatcher()
-	watcher.AddPin(s.Pin)
+	watcher.AddPin(uint(s.Pin))
 
 	for e := range watcher.Notification {
 		log.WithFields(log.Fields{
 			"pin":   e.Pin,
 			"value": e.Value,
-		}).Info("Pin triggered")
-	}
+		}).Debug("Pin triggered")
 
+		if e.Value == 1 { // High
+			log.Info("Doorbell pin went high, triggering!")
+			s.doorbellPressed()
+		}
+	}
 }
